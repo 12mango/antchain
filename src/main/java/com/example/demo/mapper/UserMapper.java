b45671f6868bd2c358ca5bdbc476ea3a3bb84010
12mango <1498159938@qq.com>
2021-07-16 10:06:57 +0800

    test2

---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.User;
+
+/**
+ * @author 12mango
+ * @since 2021/7/16
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+/**
+ * @author 12mango
+ * @since 2021/7/16
+ */
+public interface UserService {
+}
